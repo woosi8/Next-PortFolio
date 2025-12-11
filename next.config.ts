b9573088a7463666f6975b0b6e83c3bd44f73dbb
Asher(Hyuck Choi) <48211579+woosi8@users.noreply.github.com>
2025-12-11 16:29:11 +0900

    빌드 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  devIndicators: false,
+devIndicators: {
+    buildActivity: false, 
+  },
   env: {
     VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY,
   },
